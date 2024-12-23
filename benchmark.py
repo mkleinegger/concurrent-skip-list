9bddad149c6a5f21409740581d3e639d7acbc297
mkleinegger@gmail.com <mkleinegger@gmail.com>
2024-12-23 18:58:40 +0100

    update benchmarking

--- benchmark.py
+++ benchmark.py
@@ -2,36 +2,53 @@ import ctypes
 import os
 import datetime
 
-class cBenchCounters(ctypes.Structure):
-    '''
-    This has to match the returned struct in library.c
-    '''
-    _fields_ = [ ("total_inserts", ctypes.c_ulonglong),
-                 ("successful_inserts", ctypes.c_ulonglong),
-                 ("total_deletes", ctypes.c_ulonglong),
-                 ("successful_deletes", ctypes.c_ulonglong),
-                 ("total_contains", ctypes.c_ulonglong),
-                 ("successful_contains", ctypes.c_ulonglong) ]
-    
+
 class cBenchResult(ctypes.Structure):
-    '''
+    """
     This has to match the returned struct in library.c
-    '''
-    _fields_ = [ ("time", ctypes.c_float),
-                 ("counters", cBenchCounters) ]
+    """
+
+    _fields_ = [
+        ("time", ctypes.c_float),
+        ("total_inserts", ctypes.c_ulonglong),
+        ("successful_inserts", ctypes.c_ulonglong),
+        ("total_deletes", ctypes.c_ulonglong),
+        ("successful_deletes", ctypes.c_ulonglong),
+        ("total_contains", ctypes.c_ulonglong),
+        ("successful_contains", ctypes.c_ulonglong),
+    ]
+
 
 class Benchmark:
-    '''
+    """
     Class representing a benchmark. It assumes any benchmark sweeps over some
     parameter xrange using the fixed set of inputs for every point. It simply
     averages the results over the given amount of repetitions.
-    '''
-    def __init__(self, bench_function, parameters,
-                 repetitions_per_point, xrange, basedir, name):
+    """
+
+    def __init__(
+        self,
+        bench_function,
+        repetitions_per_point,
+        num_of_threads,
+        base_range,
+        runtime_in_sec,
+        operations_mix,
+        disjoint_range,
+        selection_strategy,
+        seed,
+        basedir,
+        name,
+    ):
         self.bench_function = bench_function
-        self.parameters = parameters
         self.repetitions_per_point = repetitions_per_point
-        self.xrange = xrange
+        self.num_of_threads = num_of_threads
+        self.base_range = base_range
+        self.runtime_in_sec = runtime_in_sec
+        self.operations_mix = operations_mix
+        self.disjoint_range = disjoint_range
+        self.selection_strategy = selection_strategy
+        self.seed = seed
         self.basedir = basedir
         self.name = name
 
@@ -39,66 +56,118 @@ class Benchmark:
         self.now = None
 
     def run(self):
-        '''
+        """
         Runs the benchmark with the given parameters. Collects
         repetitions_per_point data points and writes them back to the data
         dictionary to be processed later.
-        '''
+        """
         self.now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         print(f"Starting Benchmark run at {self.now}")
 
-        for x in self.xrange:
+        for t in self.num_of_threads:
             tmp = []
-            for r in range(0, self.repetitions_per_point):
-                result = self.bench_function( x, *self.parameters ).time*1000
-                tmp.append( result )
-            self.data[x] = tmp
+            for _ in range(0, self.repetitions_per_point):
+                result = self.bench_function(
+                    ctypes.c_int(t),
+                    ctypes.c_int(self.runtime_in_sec),
+                    ctypes.c_float(self.operations_mix[0]),
+                    ctypes.c_float(self.operations_mix[1]),
+                    ctypes.c_float(self.operations_mix[2]),
+                    ctypes.c_int(self.base_range[0]),
+                    ctypes.c_int(self.base_range[1]),
+                    ctypes.c_int(1 if self.disjoint_range else 0),
+                    ctypes.c_int(self.selection_strategy),
+                    ctypes.c_int(self.seed),
+                )
+
+                tmp.append(
+                    {
+                        "time": result.time,
+                        "total_inserts": result.total_inserts,
+                        "successful_inserts": result.successful_inserts,
+                        "total_deletes": result.total_deletes,
+                        "successful_deletes": result.successful_deletes,
+                        "total_contains": result.total_contains,
+                        "successful_contains": result.successful_contains,
+                    }
+                )
+
+            self.data[t] = tmp
 
     def write_avg_data(self):
-        '''
+        """
         Writes averages for each point measured into a dataset in the data
         folder timestamped when the run was started.
-        '''
+        """
         if self.now is None:
             raise Exception("Benchmark was not run. Run before writing data.")
 
-        try:
-            os.makedirs(f"{self.basedir}/data/{self.now}/avg")
-        except FileExistsError:
-            pass
-        with open(f"{self.basedir}/data/{self.now}/avg/{self.name}.data", "w")\
-                as datafile:
-            datafile.write(f"x datapoint\n")
+        # Create directory if it doesn't exist
+        avg_dir = f"{self.basedir}/data/{self.now}/avg"
+        os.makedirs(avg_dir, exist_ok=True)
+
+        # Open file for writing
+        with open(f"{avg_dir}/{self.name}.data", "w") as datafile:
+            datafile.write(
+                "threads time total_inserts successful_inserts total_deletes successful_deletes total_contains successful_contains\n"
+            )
             for x, box in self.data.items():
-                datafile.write(f"{x} {sum(box)/len(box)}\n")
+                # Calculate averages for each metric
+                avg_time = sum([entry["time"] for entry in box]) / len(box)
+                avg_total_inserts = sum(
+                    [entry["total_inserts"] for entry in box]
+                ) / len(box)
+                avg_successful_inserts = sum(
+                    [entry["successful_inserts"] for entry in box]
+                ) / len(box)
+                avg_total_deletes = sum(
+                    [entry["total_deletes"] for entry in box]
+                ) / len(box)
+                avg_successful_deletes = sum(
+                    [entry["successful_deletes"] for entry in box]
+                ) / len(box)
+                avg_total_contains = sum(
+                    [entry["total_contains"] for entry in box]
+                ) / len(box)
+                avg_successful_contains = sum(
+                    [entry["successful_contains"] for entry in box]
+                ) / len(box)
+
+                # Write averages as a single row
+                datafile.write(
+                    f"{x} {avg_time} {avg_total_inserts} {avg_successful_inserts} "
+                    f"{avg_total_deletes} {avg_successful_deletes} {avg_total_contains} {avg_successful_contains}\n"
+                )
+
 
 def benchmark():
-    '''
+    """
     Requires the binary to also be present as a shared library.
-    '''
+    """
     basedir = os.path.dirname(os.path.abspath(__file__))
-    binary = ctypes.CDLL( f"{basedir}/library.so" )
+    binary = ctypes.CDLL(f"{basedir}/library.so")
     # Set the result type for each benchmark function
     binary.small_bench.restype = cBenchResult
 
-    # The number of threads. This is the x-axis in the benchmark, i.e., the
-    # parameter that is 'sweeped' over.
-    num_threads = [3] #,2,4,8,16]#,32,64,128,256]
+    num_threads = [1]  # ,2,4,8,16]#,32,64,128,256]
 
-    # Parameters for the benchmark are passed in a tuple, here (1000,). To pass
-    # just one parameter, we cannot write (1000) because that would not parse
-    # as a tuple, instead python understands a trailing comma as a tuple with
-    # just one entry.
-    smallbench_10 = Benchmark(
-        bench_function=binary.small_bench, 
-        xrange= num_threads, 
-        parameters=(100000,),
-        repetitions_per_point=3,
-        basedir=basedir, 
-        name="smallbench_10")
+    smallbench = Benchmark(
+        bench_function=binary.small_bench,
+        num_of_threads=num_threads,
+        base_range=(0, 100000),
+        runtime_in_sec=1,
+        operations_mix=(10, 10, 80),
+        disjoint_range=False,
+        selection_strategy=0,
+        seed=42,
+        repetitions_per_point=10,
+        basedir=basedir,
+        name="smallbench_10",
+    )
 
-    smallbench_10.run()
-    smallbench_10.write_avg_data()
+    smallbench.run()
+    smallbench.write_avg_data()
+
 
 if __name__ == "__main__":
     benchmark()
--- src/library.c
+++ src/library.c
@@ -6,21 +6,18 @@
 
 #include "skiplist.h"
 
+#define VERBOSE
+
 /* These structs should to match the definition in benchmark.py */
-struct counters
+struct bench_result
 {
+    float time;
     unsigned long long total_inserts;
     unsigned long long successful_inserts;
     unsigned long long total_deletes;
     unsigned long long successful_deletes;
     unsigned long long total_contains;
-    unsigned long long successful_contains;
-};
-struct bench_result
-{
-    float time;
-    struct counters counters;
-};
+    unsigned long long successful_contains;};
 
 struct bench_result run_benchmark(
     skiplist *list,
@@ -30,44 +27,55 @@ struct bench_result run_benchmark(
     float c,
     int start_range,
     int end_range,
+    int selection_strategy,
     int seed,
     int *correctness)
 {
-    struct bench_result data = {0}; // everything set to zero
-    data.time = 0.0f;
+    struct bench_result counters = {0, 0, 0, 0, 0, 0, 0};
     srand(seed);
 
-    double tic = omp_get_wtime();
-    double toc = omp_get_wtime();
+    long key = 0;
+    double tic, toc;
+    tic = toc = omp_get_wtime();
     while (toc - tic < runtime_in_sec)
     {
-        // pick an op: (insert, remove, contains)
-        int r = rand() % 100;       // 0..99
-        long key = rand() % 100000; // pick some key range, e.g. 0..9999
-
-        if (r < 20)
+        switch (selection_strategy)
         {
+        case 0:
+            key = rand() % end_range + start_range;
+            break;
+        case 1:
+            key++;
+        default:
+            break;
+        }
+
+        int r = rand() % 100 + 1;
+        if (r <= i)
+        {
+            // avoid duplicates
             if (correctness[key] == 0)
             {
-
-                // insert
-                data.counters.total_inserts++;
+                counters.total_inserts++;
                 int success = add(list, key, NULL);
+
                 if (success)
                 {
-                    correctness[key]++;
-                    data.counters.successful_inserts++;
+                    correctness[key] = 1;
+                    counters.successful_inserts++;
                 }
             }
         }
-        else if (r < 40)
+        else if (r <= i + d)
         {
             // delete
-            data.counters.total_deletes++;
+            counters.total_deletes++;
             int success = rem(list, key);
+
+            // check for correctness
             if (success == correctness[key])
             {
-                data.counters.successful_deletes++;
+                counters.successful_deletes++;
             }
             if (success)
             {
@@ -77,20 +85,24 @@ struct bench_result run_benchmark(
         else
         {
             // contains
-            data.counters.total_contains++;
+            counters.total_contains++;
             int success = con(list, key);
+
+            // check for correctness
             if (success == correctness[key])
-                data.counters.successful_contains++;
+            {
+                counters.successful_contains++;
+            }
         }
         toc = omp_get_wtime();
     }
-    data.time = (float)(toc - tic);
-    return data;
+
+    counters.time = toc - tic;
+    return counters;
 }
 
 struct bench_result small_bench(
     int num_of_threads,
-    int repetitions,
     int runtime_in_sec,
     float i,
     float d,
@@ -102,7 +114,7 @@ struct bench_result small_bench(
     int seed)
 {
     struct bench_result result;
-    memset(&result.counters, 0, sizeof(result.counters));
+    memset(&result, 0, sizeof(result));
 
     int *correctness = malloc(100000 * sizeof(int));
     memset(correctness, 0, sizeof(correctness));
@@ -110,18 +122,19 @@ struct bench_result small_bench(
     skiplist *mylist = malloc(sizeof(skiplist));
     init(mylist); // your skiplist init
 
-    run_benchmark(mylist, 1, 0.5, 0.2, 0.3, 0, 100000, 0, correctness);
+    result = run_benchmark(mylist, runtime_in_sec, i, d, c, start_range, end_range, selection_strategy, seed, correctness);
+    
+    clean(mylist);
 
-    clean(mylist); // optional: free memory
-
+#ifdef VERBOSE
     // Print or return the result
-    printf("Thread=%d, times=%d, took=%.6f s\n", 0, 1, result.time);
-    printf("  Inserting:  %llu/%llu\n",
-           result.counters.successful_inserts, result.counters.total_inserts);
-    printf("  Deleting:   %llu/%llu\n",
-           result.counters.successful_deletes, result.counters.total_deletes);
-    printf("  Containing: %llu/%llu\n",
-           result.counters.successful_contains, result.counters.total_contains);
+    printf("\nInserting:  %llu/%llu\n",
+           result.successful_inserts, result.total_inserts);
+    printf("Deleting:   %llu/%llu\n",
+           result.successful_deletes, result.total_deletes);
+    printf("Containing: %llu/%llu\n",
+           result.successful_contains, result.total_contains);
+#endif
 
     return result;
 }
